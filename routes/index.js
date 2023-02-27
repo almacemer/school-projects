814ab360755675825f8122f1ea8beb8e3e0d2058
Alma Ćemer <126018928+almacemer@users.noreply.github.com>
2023-02-27 17:53:54 +0100

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const predavacKontroler = require('../controllers/predavac');
-
-router.get('/', predavacKontroler.provjeriNeAutentifikaciju, function(req, res, next) {
-  res.render('index', { naslov: 'Slido', kod_nije_validan: '' });
-});
-
-module.exports = router;
