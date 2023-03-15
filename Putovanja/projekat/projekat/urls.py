f305284807be400e5228664f006824f41c55766d
Alma Ćemer <126018928+almacemer@users.noreply.github.com>
2023-03-15 14:30:59 +0100

    Delete Putovanja directory

--- Putovanja/projekat/projekat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('auth', obtain_auth_token),
-]
